c4da2e3f6d1ebe826a8ead9e983bb85f015d07af
00IRCA <ismaelruizcarlin4@gmail.com>
2024-02-11 20:50:36 +0100

    fake router based on useState

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import "./Componentes/Buttons/Button.css"
 import UseStateSection from './Hooks/useState/useStateSection';
 import UseReducerSection from './Hooks/useReducer/useReducerSection';
 import Page from './Componentes/Nested/Page'
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import UseEffectContainer from './Hooks/useEffect/useEffectContainer';
 
 export const DataContext = createContext(null)
 
 function App() {
-
+  const [section, setSection] = useState("state")
   const itemsList = ["pera", "manzana", "sandia", "melocoton"]
+
+  function renderSection() {
+    switch (section) {
+      case "state": return <UseStateSection />
+      case "reducer": return <UseReducerSection />
+      case "effect": return <UseEffectContainer />
+    }
+  }
 
   return (
     <div className="App">
@@ -20,21 +28,14 @@ function App() {
         <h1>Aprende React</h1>
       </header>
       <div className='content'>
-        <h2>Hooks</h2>
+        <h2>Router</h2>
+        <ul style={{display: "flex", gap: "20px"}}>
+          <li><a onClick={() => {setSection("state")}}>Use State</a></li>
+          <li><a onClick={() => {setSection("reducer")}}>Use Reducer</a></li>
+          <li><a onClick={() => {setSection("effect")}}>Use Effect</a></li>
+        </ul>
 
-        {/*useState*/}
-        <UseStateSection />
-
-        {/*useReducer*/}
-        <UseReducerSection />
-
-        {/*createContext - useContext*/}
-        <DataContext.Provider value={itemsList}>
-          <Page />
-        </DataContext.Provider>
-
-        {/*useEffect*/}
-        <UseEffectContainer />
+        {renderSection()}
       </div>
     </div>
   );
